e5b43c2d2584c05a083084ea902ae32649aa2b7a
phreakyphoenix <adityajyotipaul007@gmail.com>
2020-05-08 04:24:11 +0530

    add day 2

---
+++ 2 Jewels and Stones (771)/soln.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        return sum(S.count(jewel) for jewel in J)
