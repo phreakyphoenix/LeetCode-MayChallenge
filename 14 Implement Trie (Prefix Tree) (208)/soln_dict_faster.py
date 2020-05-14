1391e26f7c81ec57b837164262592afa23c676d1
phreakyphoenix <adityajyotipaul007@gmail.com>
2020-05-15 02:23:59 +0530

    add faster dict soln

--- 14 Implement Trie (Prefix Tree) (208)/soln_dict_faster.py
+++ 14 Implement Trie (Prefix Tree) (208)/soln_dict_faster.py
@@ -1,64 +1,39 @@
-from collections import defaultdict
-
-class TrieNode():
-
-    def __init__(self):
-        self.children = defaultdict()
-        self.terminating = False 
-
 class Trie:
 
     def __init__(self):
-        """
-        Initialize your data structure here.
-        """
-        self.root = TrieNode()
-    
-    
-    def insert(self, word: str) -> None:
-        """
-        Inserts a word into the trie.
-        """
-        root = self.root
-
-        for letter in word:
-            if letter not in root.children:
-                root.children[letter] = TrieNode()
-            root = root.children[letter]
-
-        root.terminating = True
-
+        self.trie = {}
         
 
+    def insert(self, word: str) -> None:
+        root = self.trie
+        for c in word:
+            if c not in root:
+                root[c]={}
+            root = root[c]
+        root['#'] = '#'
+
     def search(self, word: str) -> bool:
-        """
-        Returns if the word is in the trie.
-        """
-        root = self.root
-
-        for letter in word:
-            if letter not in root.children:
-                return False
-            root = root.children[letter]
-
-        return True if root.terminating else False
+        # start = self.trie
+        # for c in word:
+        #     if c not in start:
+        #         return False
+        #     start = start[c]
+        # if '#' in start:
+        #     return True
+        # return False
+        return self.startsWith(word + '#')
 
     def startsWith(self, prefix: str) -> bool:
-        """
-        Returns if there is any word in the trie that starts with the given prefix.
-        """
-        root = self.root
-
-        for letter in prefix:
-            if letter not in root.children:
+        start = self.trie
+        for c in prefix:
+            if c not in start:
                 return False
-            root = root.children[letter]
+            start = start[c]
+        return True
 
-        return True 
-obj = Trie()
-print (obj.insert("apple"))
-print (obj.search("apple"))   # returns true
-# print (obj.search("app"))    # returns false
-print (obj.startsWith("app")) # returns true
-# print (obj.insert("app"))   
-# print (obj.search("app"))     # returns true
+
+# Your Trie object will be instantiated and called as such:
+# obj = Trie()
+# obj.insert(word)
+# param_2 = obj.search(word)
+# param_3 = obj.startsWith(prefix)
